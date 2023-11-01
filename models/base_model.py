2bf4cf4d5c7faf1839b83d1bcf38f40d53cedf5f
Jean-Luc <jlucbilo@gmail.com>
2023-11-02 00:26:55 +0100

    creation of an instance with the dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -15,10 +15,26 @@ class BaseModel:
     created_at = datetime.now()
     update_at = str(uuid.uuid4())
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Updating datetile"""
-        self.updated_at = datetime.now()
-        self.id = str(uuid.uuid4())
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "created_at":
+                    self.created_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "my_number":
+                    self.my_number = value
+                if key == "name":
+                    self.name = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ print a specifique format of informations nexpected"""
@@ -27,16 +43,13 @@ class BaseModel:
 
     def save(self):
         """ Update the instance attribut update_at"""
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all keyand value"""
         my_dic = self.__dict__.copy()
         my_dic['__class__'] = self.__class__.__name__
-
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        self.created_at = self.created_at.isoformat(self.created_at)
-        self.updated_at = self.updated_at.isoformat(self.updated_at)
+        my_dic['created_at'] = datetime.isoformat(self.created_at)
+        my_dic['updated_at'] = datetime.isoformat(self.updated_at)
 
         return my_dic
