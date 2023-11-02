f576eac389f5f9395360df088cdff95a088dc406
Jean-Luc <jlucbilo@gmail.com>
2023-11-02 11:26:17 +0100

    fixing indentation's issues

--- console.py
+++ console.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""The programm that contains the entry
-point of the command interpreter
+"""The program that contains the entry point of the command interpreter
 """
-
 
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-   """class that difine the console behavior"""
-    prompt = "(hbnb)"
-    print("{}{}".format('Documented commands (type help <topic>):\n', ('=' * 40)))
+    """Class that defines the console behavior"""
+    prompt = "(hbnb) "
 
     def do_EOF(self, arg):
-	    """EOF to exit the program"""
+        """EOF to exit the program"""
         return True
 
-    def quit(self, line)
-	""" 'quit' command to exit the program"""
+    def do_quit(self, arg):
+        """'quit' command to exit the program"""
         return True
 
     def emptyline(self):
-        """do nothing when we have empty line, or space"""
+        """"Documented commands (type help <topic>):
+            =======================================
+            EOF    help    quit
+        """
         pass
 
 
