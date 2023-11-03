2353c04a013a89710ffae4075322809f535474ae
Holbiwan <6188@holbertonstudents.com>
2023-11-03 16:17:06 +0100

    Unittest for state module

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Unittest for State class"""
+
+import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    def test_default_name_attribute(self):
+        """Test the default name attribute of a State instance."""
+        state = State()
+        self.assertEqual(state.name, "")
+
+    def test_instance_creation(self):
+        """Test if a State instance can be created."""
+        state = State()
+        self.assertIsInstance(state, State)
+
+    def test_name_attribute_type(self):
+        """Test the data type of the name attribute of a State instance."""
+        state = State()
+        self.assertIsInstance(state.name, str)
+
+if __name__ == '__main__':
+    unittest.main()
