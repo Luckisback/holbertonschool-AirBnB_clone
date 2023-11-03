c13d008170a50f3c35c2a8b430b6d00b11348f46
Holbiwan <6188@holbertonstudents.com>
2023-11-03 14:33:10 +0100

    Fix errors on code

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""Unittest for city class"""
-
+"""test for city"""
 import unittest
 from models.city import City
 
-
 class TestCity(unittest.TestCase):
-    """testing the correct functioning of the class"""
-    def test_attr(self):
-        """test attributes"""
+    def test_instance_creation(self):
+        """Test if a City instance can be created."""
         city = City()
-        self.assertEqual(city.state_id, "")
-        self.assertEqual(city.name, "")
+        self.assertIsInstance(city, City)
 
-    def test_id(self):
-        "Test id"
+    def test_default_name(self):
+        """Test if the default city name is an empty string."""
+        city = City()
+        self.assertEqual("", city.name)
+
+    def test_id_type(self):
+        """Test if the type of city's ID is a string."""
         city = City()
         self.assertEqual(str, type(city.id))
+
+    def test_default_state_id(self):
+        """Test if the default state_id is an empty string."""
+        city = City()
+        self.assertEqual("", city.state_id)
+
+if __name__ == '__main__':
+    unittest.main()
+
