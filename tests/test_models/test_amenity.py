450bb23975756324a0561e9acb6f7ff851996f3c
Jean-Luc <jlucbilo@gmail.com>
2023-11-03 10:28:04 +0100

    Unittest carried out on the file amenity

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" unittest for amenity file """
+from models.base_model import BaseModel
+from models.amenity import Amenity
+import unittest
+
+
+class Test_Amenity(unittest.TestCase):
+    """ unittest for amenity class """
+    def test_amenity(self):
+        amenity = Amenity()
+        self.assertEqual("", amenity.name)
