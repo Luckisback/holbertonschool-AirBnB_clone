9a1b6d58c846d059136ecfb37acf54ce07967528
Holbiwan <6188@holbertonstudents.com>
2023-11-03 16:28:24 +0100

    Unittest for user module

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest for User class"""
+
+import unittest
+from models.user import User
+
+class TestUser(unittest.TestCase):
+    def test_default_attributes(self):
+        """Test the default attributes of a User instance."""
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+
+    def test_attribute_types(self):
+        """Test the data types of User attributes."""
+        user = User()
+        self.assertIsInstance(user.email, str)
+        self.assertIsInstance(user.password, str)
+        self.assertIsInstance(user.first_name, str)
+        self.assertIsInstance(user.last_name, str)
+
+if __name__ == "__main__":
+    unittest.main()   
